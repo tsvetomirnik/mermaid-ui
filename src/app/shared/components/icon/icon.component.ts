913dfb78a45e7aadbdfd388ba030c5dbd4d61fbc
Tsvetomir Nikolov <cvetomir.nikolov@gmail.com>
2022-08-23 22:30:16 +0300

    Deploy

--- src/app/shared/components/icon/icon.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-icon',
-  templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class IconComponent {}
